40935429420ecfe92b14fef75d85041244ed5bc1
Marris <37194106+Marrislul@users.noreply.github.com>
2024-04-26 00:55:43 +0200

    update 1.2

---
+++ ByeByeGryphons.lua
@@ -0,0 +1,14 @@
+--Local function to set the variables
+local function loadSettings(self, event, ...)
+	MainMenuBar.EndCaps.LeftEndCap:Hide() --left
+	MainMenuBar.EndCaps.RightEndCap:Hide() --right
+end
+
+--Create frame, needed for trigger & changing the variables.
+local frame = CreateFrame("Frame")
+--Trigger for event, entering the world in this case.
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+--Loads settings one the event is triggered.
+frame:SetScript("OnEvent", loadSettings)
+--Prints info (personal preference). If you check this code and don't want this, just remove it.
+print("Bye Bye Gryphons: Gryphons removed.")
